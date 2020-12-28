65dd060b3b45479b08556d76e69e46e2bb4288bc
Lee Hanhyeong <forestcamp11@gmail.com>
2020-12-28 14:09:50 +0900

    first

---
+++ ListBoxSelectedPopupButton/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBoxSelectedPopupButton
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lb.Items.Add(new ListBoxItem() {Content = "가나다라마바사 1" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 2" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 3" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 4" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 5" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 6" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 7" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 8" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 9" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 10" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 11" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 12" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 13" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 14" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 15" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 16" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 17" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 18" });
+            lb.Items.Add(new ListBoxItem() { Content = "가나다라마바사 19" });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            popup.IsOpen = !popup.IsOpen;
+        }
+
+                      
+
+        private void btnLBItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var listItem = (sender as Button).TemplatedParent as ListBoxItem;
+                if (listItem != null)
+                {
+                    listItem.IsSelected = true;
+                    popup.IsOpen = !popup.IsOpen;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }            
+        }
+    }
+}
